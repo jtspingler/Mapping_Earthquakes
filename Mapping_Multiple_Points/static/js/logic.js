0082bfacef47e2a5eef43f612d3462cb49418f2e
jtspingler <jackson.spingler@guildeducation.com>
2022-08-06 16:20:16 -0600

    13.4.3

---
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -0,0 +1,7 @@
+// Get data from cities.js
+let cityData = cities;
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+    console.log(city)
+    L.marker(city.location).addTo(map);
+});
